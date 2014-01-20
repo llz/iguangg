36ebca44c3947f1e6f2c878a43b62d46fa25bfd8
llz <l.l.z@live.cn>
2014-01-20 11:05:10 +0800

    init

---
+++ app/Lib/Cache/Engine/BaeMemcacheEngine.php
@@ -0,0 +1,35 @@
+<?php
+class BaeMemcacheEngine extends CacheEngine{
+	public function clear($check) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public function decrement($key, $offset = 1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public function delete($key) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public function increment($key, $offset = 1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public function read($key) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public function write($key, $value, $duration) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+}
